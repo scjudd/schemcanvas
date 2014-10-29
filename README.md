26cb3fa74a3f377b60ac7dc8f824888d6e5cf43e
Spencer Judd <spencercjudd@gmail.com>
2014-10-29 11:15:07 -0400

    Component factory and SchemCanvas stub in place.
    It's possible to draw Components on a SchemCanvas instance, but
    SchemCanvas doesn't yet keep track of any state.
    
    See the bottom of index.html for an API example.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SchemCanvas
+
+Circuit Symbols courtesy of [Matthew Beckler][circuit-symbols].
+
+[circuit-symbols]: http://www.mbeckler.org/inkscape/circuit_symbols/
---
+++ schemcanvas.js
@@ -0,0 +1,43 @@
+var Components = (function() {
+  var Component = {
+    create: function(imageUrl) {
+      var that = Object.create(this);
+      that.image = new Image();
+      that.image.src = imageUrl;
+
+      var callbacks = [];
+      var loaded = false;
+
+      that.image.onload = function() {
+        loaded = true;
+        for (var i in callbacks) callbacks[i]();
+        callbacks = [];
+      };
+
+      this.onceLoaded = function(callback) {
+        if (loaded) callback();
+        else callbacks.push(callback);
+      };
+
+      return that;
+    },
+
+    draw: function(ctx, x, y) {
+      this.onceLoaded(function() {
+        ctx.drawImage(this.image, x, y);
+      }.bind(this));
+    }
+  };
+
+  return {
+    LED: Component.create('led.png'),
+  };
+})();
+
+function SchemCanvas(elem) {
+  this.ctx = elem.getContext('2d');
+}
+
+SchemCanvas.prototype.drawComponent = function(component, x, y) {
+  return component.draw(this.ctx, x, y);
+};
