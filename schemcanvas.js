19d163d3b14abe033518b6825bd4d01fa68da68b
Spencer Judd <spencercjudd@gmail.com>
2014-10-29 16:47:10 -0400

    Added Component position tracking to SchemCanvas.

--- schemcanvas.js
+++ schemcanvas.js
@@ -37,8 +37,24 @@ var Components = (function() {
 function SchemCanvas(elem) {
   this.elem = elem;
   this.ctx = elem.getContext('2d');
+  this.components = [];
 }
 
-SchemCanvas.prototype.drawComponent = function(component, x, y) {
-  return component.draw(this.ctx, x, y);
+SchemCanvas.prototype.addComponent = function(component, x, y) {
+  var c = Object.create(component);
+
+  // Center component on the provided x, y values
+  c.x = x - Math.floor(c.image.width / 2);
+  c.y = y - Math.floor(c.image.height / 2);
+
+  this.components.push(c);
+  this.refresh();
+};
+
+SchemCanvas.prototype.refresh = function() {
+  this.ctx.clearRect(0, 0, this.elem.width, this.elem.height);
+  for (var i in this.components) {
+    var c = this.components[i];
+    c.draw(this.ctx, c.x, c.y);
+  }
 };
